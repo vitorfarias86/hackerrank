50a0291e62e27ce49934bbc6bc918a9ff3081f06
vitorfarias86 <vitorfarias86@gmail.com>
2017-08-24 18:21:51 -0300

    teste

--- MagicNumber.java
+++ MagicNumber.java
@@ -1,8 +1,8 @@
 package br.com.rackerrank.solution1;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -11,8 +11,8 @@ import java.util.Set;
 
 public class MagicNumber {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedReader br = new BufferedReader(new FileReader("/home/vitor/Desktop/magic-number-testcases/input/input01.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedReader br = new BufferedReader(new FileReader("/home/vitor/Desktop/magic-number-testcases/input/input01.txt"));
 		// InputStreamReader(System.in));
 		String linha = br.readLine();
 		int T = Integer.parseInt(linha);
